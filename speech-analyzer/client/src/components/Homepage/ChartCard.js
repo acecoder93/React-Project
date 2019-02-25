e9b1e405f5665f09a312cdfead65bbe8ac6fdf6e
Anuj <anujsaheba@gmail.com>
2019-02-24 19:22:13 -0600

    design in-progress

--- speech-analyzer/client/src/components/Homepage/ChartCard.js
+++ speech-analyzer/client/src/components/Homepage/ChartCard.js
@@ -6,10 +6,11 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Chart from 'chart.js';
 
 const styles = {
   card: {
-    minWidth: 275,
+    maxWidth: 400,
   },
   bullet: {
     display: 'inline-block',
@@ -24,6 +25,8 @@ const styles = {
   },
 };
 
+
+
 function ChartCard(props) {
   const { classes } = props;
   const bull = <span className={classes.bullet}>•</span>;
@@ -32,27 +35,12 @@ function ChartCard(props) {
     <Card className={classes.card}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          Chart Breakdown
         </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+
+ 
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+     
     </Card>
   );
 }
--- speech-analyzer/client/src/components/Homepage/WatsonCard.js
+++ speech-analyzer/client/src/components/Homepage/WatsonCard.js
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography';
 
 const styles = {
   card: {
-    minWidth: 275,
+    maxWidth: 400,
   },
   bullet: {
     display: 'inline-block',
@@ -30,32 +30,19 @@ function WatsonCard(props) {
 
   return (
     <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <CardContent>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+        Watson Breakdown
+      </Typography>
+
+
+    </CardContent>
+   
+  </Card>
   );
 }
+
+
 
 WatsonCard.propTypes = {
   classes: PropTypes.object.isRequired,

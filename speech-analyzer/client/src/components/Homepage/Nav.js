bf35750972add73a30251a154ebc7a87807701b7
Anuj <anujsaheba@gmail.com>
2019-02-24 18:07:22 -0600

    updated with cards and form

--- speech-analyzer/client/src/components/Homepage/Nav.js
+++ speech-analyzer/client/src/components/Homepage/Nav.js
@@ -31,9 +31,7 @@ function ButtonAppBar(props) {
         <Toolbar className="navBar">
           {/* <Typography variant="h6" color="inherit" className={classes.grow}>Speech Analyzer</Typography> */}
           <Button color="inherit"><Link to="/" className="navButton">Home</Link></Button>
-          <Button color="inherit"><Link to="/famous" className="navButton">Famous Speeches</Link></Button>
           <Button color="inherit"><Link to="/custom" className="navButton">Custom</Link></Button>
-          <Button color="inherit"><Link to="/feedback" className="navButton">Feedback</Link></Button>
         </Toolbar>
       </AppBar>
     </div>
